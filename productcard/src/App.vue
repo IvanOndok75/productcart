d71fc633bfc92db64538935682050751342c53a2
IvanOndok75 <kotikpusistik417@gmail.com>
2025-09-29 18:19:30 +0300

    a

--- productcard/src/App.vue
+++ productcard/src/App.vue
@@ -1,12 +1,6 @@
 <template>
 <div id="app">
-<ProductList
-name="Українська вишиванка"
-description="Стильна вишиванка з традиційними орнаментами."
-price="1500"
-image="https://recommerce.com.ua/static/kozachok.reshop.com.ua/catalog/381/195715475364252e14cc6cb_medium.jpg"
-/>
-<ReviewsComponent/>
+
 </div>
 </template>
 
